73793677365081c3435378670841e9339627937e
Kyle <91167220+kylet719@users.noreply.github.com>
2023-12-07 12:14:32 -0800

    Update id.py

--- id.py
+++ id.py
@@ -6,6 +6,6 @@ twilioPhoneNo = "+" # In +CountryCode format i.e +16041234567
 # Fill with the phone number you want to send your texts too
 phoneNo = "+" # In +CountryCode format i.e +16041234567
 
-# Fill in with market open & close
+# Fill in with market open & close (in PST)
 marketOpen = "6:30"
 marketClose = "13:00"
